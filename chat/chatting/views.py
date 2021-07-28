bbb78aab95e546a4f4a790e12681fea071508960
Tushar <tusharverma1510@gmail.com>
2021-07-28 14:00:10 +0530

    push

---
+++ chat/chatting/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+from .models import chattingkre 
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def chatting(request):
+    Name = request.POST.get('name', 'No')
+    Text=request.POST.get('message', 'No')
+    
+    create=chattingkre(name=Name,text=Text)
+    create.save()
+
+    catprods = chattingkre.objects.all()
+    n=len(catprods)
+    print(catprods)
+    details={'name':Name,'text':Text,'catprods':catprods,'range':range(1,n)}
+    return render(request,'chatting.html',details)
